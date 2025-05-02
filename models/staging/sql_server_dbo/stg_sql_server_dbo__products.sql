d24cef8b20c8ee9930c55bf17db4e2338d9096be
Jose Pilartes <pmaga5821@gmail.com>
2025-05-02 01:23:13 +0000

    create cte´s for staging models

--- models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
@@ -1,2 +1,17 @@
+with src_data as (
+    select *
+    from {{ source("sql_server_dbo" , "products")}}
+),
+casted_renamed as(
+    select 
+         PRODUCT_ID 
+        , PRICE::decimal(10,4) as PRICE
+        , NAME 
+        , INVENTORY
+        , _FIVETRAN_DELETED 
+        , _FIVETRAN_SYNCED
+    from src_data 
+)
+
 select *
-from {{ source("sql_server_dbo" , "products")}}
+from casted_renamed
--- models/staging/sql_server_dbo/stg_sql_server_dbo__promo.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promo.sql
@@ -1,2 +1,17 @@
+with src_data as (
+    select *
+    from {{ source("sql_server_dbo", "promos") }}
+),
+casted_renamed as (
+    select 
+        PROMO_ID
+        , DISCOUNT::decimal(5,2) as "DISCOUNT %"
+        , STATUS
+        , _FIVETRAN_DELETED
+        , _FIVETRAN_SYNCED
+    from src_data
+)
+
 select *
-from {{ source("sql_server_dbo", "promos") }}
+from casted_renamed
+
