ce3d5c4d6d7aa82963fa3e82fd8b2db4e7220152
Jose Pilartes <pmaga5821@gmail.com>
2025-05-23 09:02:37 +0000

    comment

--- models/marts/marketing/plant_popularity_by_region.sql
+++ models/marts/marketing/plant_popularity_by_region.sql
@@ -10,6 +10,7 @@ select
    , count(distinct customer_id) as unique_customers
    , approx_percentile(matched_order_count, 0.5) as median_orders_per_customer
 from product_customer_match_location
+-- getting just the order_item who matched location
 where matched_order_count > 0
 group by desc_product, product_id
 order by total_orders desc
